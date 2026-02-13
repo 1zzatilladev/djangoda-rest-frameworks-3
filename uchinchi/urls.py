3f16d16bbcb0d08e7db1678baf3f16eb8a711a50
Izzatillo <izzatillaasatullayev55@gmail.com>
2026-02-13 14:53:05 +0500

    salomim

--- uchinchi/urls.py
+++ uchinchi/urls.py
@@ -1,16 +1,16 @@
-from django.urls import path
-from .views import Salom1ApiVIew,Salom2ApiVIew,Salom3ApiVIew,Salom4ApiVIew,Salom5ApiVIew , Salom1DetailApiVIew, \
-Salom2DetailApiVIew , Salom3DetailApiVIew ,Salom4DetailApiVIew ,Salom5DetailApiVIew
-   
+from django.urls import path , include
+from . import views
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register(r"salom1" , views.Salom1ModelViewSet)
+router.register(r"salom2" , views.Salom2ModelViewSet)
+router.register(r"salom3" , views.Salom3ModelViewSet)
+router.register(r"salom4" , views.Salom4ModelViewSet)
+router.register(r"salom5" , views.Salom5MarketLogModelViewSet)
+
+
 urlpatterns = [
-    path('salom1/',Salom1ApiVIew.as_view()),
-    path('salom2/',Salom2ApiVIew.as_view()),
-    path('salom3/',Salom3ApiVIew.as_view()),
-    path('salom4/',Salom4ApiVIew.as_view()),
-    path('salom5/',Salom5ApiVIew.as_view()),
-    path('salom1/<int:pk>/', Salom1DetailApiVIew.as_view()),
-    path('salom2/<int:pk>/', Salom2DetailApiVIew.as_view()),
-    path('salom3/<int:pk>/', Salom3DetailApiVIew.as_view()),
-    path('salom4/<int:pk>/', Salom4DetailApiVIew.as_view()),
-    path('salom5/<int:pk>/', Salom5DetailApiVIew.as_view()),
+    path("",include(router.urls))
 ]
--- uchinchi/views.py
+++ uchinchi/views.py
@@ -1,55 +1,26 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializer import Salom3Sarializer,Salom1Sarializer,Salom2Sarializer,Salom4Sarializer,Salom5Serializer
 from .models import Salom3,Salom4,Salom1,Salom2,Salom5
-from .serializer import Salom1Sarializer,Salom2Sarializer,Salom3Sarializer,Salom4Sarializer,Salom5Serializer
-from rest_framework.generics import  ListCreateAPIView, \
-     RetrieveUpdateDestroyAPIView
-
-#---------------------------
-class Salom1ApiVIew(ListCreateAPIView):
-    serializer_class = Salom1Sarializer
-    queryset = Salom1.objects.all()
 
 
-class Salom1DetailApiVIew(RetrieveUpdateDestroyAPIView):
-    serializer_class = Salom1Sarializer
-    queryset = Salom1.objects.all()
 
-#---------------------------
-class Salom2ApiVIew(ListCreateAPIView):
-    serializer_class = Salom2Sarializer
-    queryset = Salom2.objects.all()
-
-
-class Salom2DetailApiVIew(RetrieveUpdateDestroyAPIView):
-    serializer_class = Salom2Sarializer
-    queryset = Salom2.objects.all()
-#----------------------
-
-class Salom3ApiVIew(ListCreateAPIView):
-    serializer_class = Salom3Sarializer
+class Salom3ModelViewSet(ModelViewSet):
     queryset = Salom3.objects.all()
-
-
-class Salom3DetailApiVIew(RetrieveUpdateDestroyAPIView):
     serializer_class = Salom3Sarializer
-    queryset = Salom3.objects.all()
 
-#-------------------------
-class Salom4ApiVIew(ListCreateAPIView):
-    serializer_class = Salom4Sarializer
+class Salom4ModelViewSet(ModelViewSet):
     queryset = Salom4.objects.all()
-
-
-class Salom4DetailApiVIew(RetrieveUpdateDestroyAPIView):
     serializer_class = Salom4Sarializer
-    queryset = Salom4.objects.all()
 
-#---------------------------------
-class Salom5ApiVIew(ListCreateAPIView):
-    serializer_class = Salom5Serializer
+class Salom1ModelViewSet(ModelViewSet):
+    queryset = Salom1.objects.all()
+    serializer_class = Salom1Sarializer
+
+
+class Salom2ModelViewSet(ModelViewSet):
+    queryset = Salom2.objects.all()
+    serializer_class = Salom2Sarializer
+
+class Salom5MarketLogModelViewSet(ModelViewSet):
     queryset = Salom5.objects.all()
-
-
-class Salom5DetailApiVIew(RetrieveUpdateDestroyAPIView):
     serializer_class = Salom5Serializer
-    queryset = Salom5.objects.all()
